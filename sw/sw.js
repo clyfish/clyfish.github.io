9d148e37a15cf816acff3573e7c074a03f001c93
Cheng Liangyu <clyfish@gmail.com>
2023-10-23 19:07:03 +0800

    cache source position table with service worker

---
+++ sw/sw.js
@@ -0,0 +1,7 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
+workbox.precaching.precacheAndRoute([{
+  url: '../echarts-5.4.3.min.js',
+  revision: null,
+}])
+workbox.core.clientsClaim()
+workbox.core.skipWaiting()
