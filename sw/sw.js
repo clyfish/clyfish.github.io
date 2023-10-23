789af4f2d7db4617266f84fa1f719c64bb9e74b5
Cheng Liangyu <clyfish@gmail.com>
2023-10-23 19:35:42 +0800

    absolute path

--- sw/sw.js
+++ sw/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
 workbox.precaching.precacheAndRoute([{
-  url: 'echarts-5.4.3.min.js',
+  url: '/sw/echarts-5.4.3.min.js',
   revision: null,
 }])
 workbox.core.clientsClaim()
